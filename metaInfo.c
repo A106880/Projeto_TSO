a446fb295d7e7ec3df8895d75fa9e46411b94bbe
Afonso_Q <169604828+A106880@users.noreply.github.com>
2026-04-04 18:06:01 +0100

    Adição do fuse passthrough e da deduplicação na escrita.

---
+++ metaInfo.c
@@ -0,0 +1,33 @@
+#include "metaInfo.h"
+
+void freeFilemeta(void *data){
+    if (!data) return;
+	filemeta *file = (filemeta*)data;
+	g_queue_free_full(file->blockList,freeBlockMeta); //value da block list é um blockmeta
+    g_free(file);
+}
+
+void freeBlockMeta(void *data){
+    if (!data) return;
+    blockmeta *block = (blockmeta *)data;
+    // free(block->in_buf);
+    if (block->id) g_free(block->id);
+    g_free(block);
+}
+
+guint blockHashFunc(gconstpointer key){
+    guint *value = (guint *) key;
+
+    guint finalKey = *value;
+
+    return finalKey;
+}
+
+
+gboolean compareSHAHashes(gconstpointer hash1, gconstpointer hash2){
+    if (memcmp(hash1, hash2, 64) == 0) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
---
+++ metaInfo.h
@@ -0,0 +1,30 @@
+#include <glib.h>
+
+
+typedef struct file{
+    char *id; //path
+    off_t realSize; //tamanho depois da deduplicacao
+    off_t logicalSize; // tamanho antes da deduplicacao
+    GQueue *blockList; // value é o blockmeta
+} filemeta;
+
+typedef struct block{
+    char *in_buf; //apontador para a localizacao em memoria do bloco
+    char *id; // id é a hash SHA-512
+    size_t size; // tamanho do bloco
+} blockmeta;
+
+// struct GHashTable fileIndex; // key -> id do ficheiro  // value -> filemeta
+
+// struct GHashTable blockCounter; // key -> id do bloco  // value -> contador
+
+// struct GHashTable partialIndex; // key -> id do bloco  // value -> bloco
+
+void freeFilemeta(void *data);
+
+void freeBlockMeta(void *data);
+
+guint blockHashFunc(gconstpointer key);
+
+gboolean compareSHAHashes(gconstpointer key1, gconstpointer key2);
+
